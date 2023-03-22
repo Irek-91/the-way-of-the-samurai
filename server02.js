9a8cb1bdf4d4019e702e62ac1abd30dec200d91f
Irek-91 <shamilov.irek@bk.ru>
2023-03-22 22:23:22 +0300

    readFileSync

--- server02.js
+++ server02.js
@@ -1,22 +1,18 @@
 const http = require('http')
+const fs = require('fs')
 
 const server = http.createServer((request, response) => {
     switch(request.url) {
         case '/home': {
-            
-            const start = new Date()
-            while (new Date() - start < 5000) {
-                console.log(new Date() - start)
-            }
-            const data = 'bset free online course'
+            const data = fs.readFileSync('tittle.html')
             response.write(data)
-            response.end()
             break;
         }
         default: {
             response.write('404 not found')
-            response.end()
         }
     }
+
+    response.end()
 })
 server.listen(3003)
